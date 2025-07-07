eae740dc9ebe40c92229dad0ce1864d985780ed2
iamakitten1 <m.kostamashvili.1@gmail.com>
2025-07-07 23:12:26 +0200

    impliment auth pages with form validations

---
+++ line-managment/src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+
+import { SignUpCard } from "@/features/auth/components/sign-up-card";
+
+export default function SignUp() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <SignUpCard />
+        </div>
+    )
+}
---
+++ line-managment/src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import Link from "next/link";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { AuthSkeleton } from "@/components/auth-skeleton";
+import { useState, useEffect } from "react";
+
+const formSchema = z.object({
+  email: z.string().email({ message: "Invalid email" }),
+  password: z.string().min(6, { message: "At least 6 characters" }),
+});
+
+export function SignInCard() {
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log("Login values:", values);
+    form.reset();
+  };
+
+  if (!isMounted) return <AuthSkeleton />;
+
+  return (
+    <Card className="w-full md:w-[487px] border-none shadow-none">
+      <CardHeader className="text-center p-7">
+        <CardTitle className="text-2xl">Welcome Back!</CardTitle>
+      </CardHeader>
+
+      <div className="px-7 mb-2">
+        <DottedSeparator />
+      </div>
+
+      <CardContent className="p-7">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input {...field} placeholder="Enter your email" type="email" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input {...field} placeholder="Enter your password" type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" className="w-full" size="lg">
+              Sign In
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+
+      <CardContent className="p-7 flex flex-col gap-y-4">
+        <Button variant="secondary" size="lg" className="w-full">
+          <FcGoogle className="mr-2 size-5" />
+          Login with Google
+        </Button>
+        <Button variant="secondary" size="lg" className="w-full">
+          <FaGithub className="mr-2 size-5" />
+          Login with Github
+        </Button>
+      </CardContent>
+
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+
+      <CardContent className="p-7 text-center">
+        Don&apos;t have an account?{" "}
+        <Link href="/sign-up" className="text-blue-500 font-semibold underline">
+          Sign Up
+        </Link>
+      </CardContent>
+    </Card>
+  );
+}
